6edf1357297520cc0f45d7f3f64c3907cd05bd80
Faizan-313 <faizansyedpeer@gmail.com>
2025-11-27 23:20:24 +0530

    add mongodb atlas

--- codefusion/backend/database/db.js
+++ codefusion/backend/database/db.js
@@ -3,7 +3,8 @@ import "dotenv/config"
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_DB_URL}/${process.env.DB_NAME}`)
+        // await mongoose.connect(`${process.env.MONGO_DB_URL}/${process.env.DB_NAME}`)  local
+        await mongoose.connect(process.env.MONGO);    //atlas
         console.log(`Database connected`);
     } catch (error) {
         console.log("Error in connecting to database: ", error)
