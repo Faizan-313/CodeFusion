656983f917c7fd09c4dc68ea984186b859124877
Faizan-313 <faizansyedpeer@gmail.com>
2025-11-28 00:53:08 +0530

    added domain

--- codefusion/backend/controllers/auth.controller.js
+++ codefusion/backend/controllers/auth.controller.js
@@ -7,7 +7,7 @@ const options = {
     httpOnly: true,
     secure: isProduction,
     sameSite: isProduction ? "none" : "lax",
-    domain: ".onrender.com", 
+    domain: isProduction ? process.env.DOMAIN : "localhost",
     maxAge: 7 * 24 * 60 * 60 * 1000
 }
 
